794724798103eff46f993cc24718df399a840544
Edgar Dev <118010644+EdgarrDEV@users.noreply.github.com>
2023-02-04 00:07:36 -0600

    04022023

--- flutter_contenedor/lib/main.dart
+++ flutter_contenedor/lib/main.dart
@@ -5,30 +5,92 @@ void main() {
 }
 
 class Contenedores extends StatelessWidget {
-//Widget que no cambia en su ejecución
+  //Widget que no cambia en su ejecución
+
+  //const Contenedores({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      width: 200,
-      color: Colors.blueAccent,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ContainesF(),
     );
   }
 }
 
-class Containers extends StatefulWidget {
+class ContainesF extends StatefulWidget {
   //si cambia de estado en su ejecución
 
-  const Containers({super.key});
+  //const ContainesF({super.key});
 
   @override
-  State<Containers> createState() => _ContainersState();
+  State<ContainesF> createState() => _ContainesFState();
 }
 
-class _ContainersState extends State<Containers> {
+class _ContainesFState extends State<ContainesF> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      //backgroundColor: Colors.deepOrange,
+      body: Column(
+        children: [
+          Container(
+            //padding: EdgeInsets.all(50),
+            // alignment: Alignment(0.9, 0.2),
+            height: 100,
+            //width: 200,
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
+              // border: Border.all(color: Colors.black, width: 10),
+              //shape: BoxShape.circle
+              borderRadius: BorderRadius.circular(5),
+              //child: Text( "Recuadro",style: TextStyle(fontSize: 20),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey.shade500,
+                    offset: Offset(4.0, 4.0),
+                    blurRadius: 15.0),
+                BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(-4.0, -4.0),
+                    blurRadius: 15.0,
+                    spreadRadius: 1.0),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              //height: 100,
+              decoration: BoxDecoration(
+                color: Colors.deepOrange,
+                borderRadius: BorderRadius.circular(5),
+                /*boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.shade500,
+                      offset: Offset(4.0, 4.0),
+                      blurRadius: 15.0),
+                  BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(-4.0, -4.0),
+                      blurRadius: 15.0,
+                      spreadRadius: 1.0),
+                ],*/
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              //height: 100,
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
