a3b84fdad3c1185dbe6e2c08f0523a6f3808485b
Edgar Dev <118010644+EdgarrDEV@users.noreply.github.com>
2023-02-11 07:13:31 -0600

    1102230713

--- flutter_list_builder/lib/figuras/square.dart
+++ flutter_list_builder/lib/figuras/square.dart
@@ -3,14 +3,45 @@ import 'package:flutter/material.dart';
 class MiFigura extends StatelessWidget {
   // const MiFigura({super.key});
 
-  @override
+  final child;
+  MiFigura({required this.child});
+
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 8.0), //primer hijo
-      child: Container(
-        height: 700,
-        color: Colors.amberAccent[200],
-      ), //Nieto
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 8.0), //primer hijo
+          child: Container(
+              height: 700,
+              color: Colors.amberAccent[200],
+              child: Image.network(
+                  "https://png.pngtree.com/thumb_back/fh260/background/20200731/pngtree-blue-carbon-background-with-sport-style-and-golden-light-image_371487.jpg")), //Nieto
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 8.0), //primer hijo
+          child: Container(
+            height: 700,
+            color: Colors.cyanAccent[200],
+            child: Center(
+                child: Text(
+              child,
+              style: TextStyle(fontSize: (40)),
+            )),
+          ), //Nieto
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 8.0), //primer hijo
+          child: Container(
+            height: 700,
+            color: Colors.blueAccent[200],
+            child: Center(
+                child: Text(
+              child,
+              style: TextStyle(fontSize: (40)),
+            )),
+          ), //Nieto
+        ),
+      ],
     );
   }
 }
--- flutter_list_builder/lib/paginas/principal.dart
+++ flutter_list_builder/lib/paginas/principal.dart
@@ -13,7 +13,62 @@ class _PrincipalState extends State<Principal> {
   Widget build(BuildContext context) {
     final List _post = ['pantalla1', 'pantalla2', 'pantalla3', 'pantalla4'];
     return Scaffold(
-      body: ListView(
+      body: Column(
+        children: [
+//history of instagram
+          Expanded(
+            child: ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.pink,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.pink,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.pink,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+//instagram post
+
+          Expanded(
+            child: ListView.builder(
+                itemCount: _post.length,
+                itemBuilder: (context, index) {
+                  return MiFigura(
+                    child: _post[index],
+                  );
+                }),
+          ),
+        ],
+      ),
+      /*/se combierte en bulder ListView(
         //Padre
         //physics: NeverScrollableScrollPhysics(), // por si no quiero scroll
 
@@ -53,7 +108,7 @@ class _PrincipalState extends State<Principal> {
           MiFigura(),
           MiFigura(),
         ],
-      ),
+      ),*/
     );
   }
 }
