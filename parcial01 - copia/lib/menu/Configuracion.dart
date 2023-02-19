3b5c96d89056c15dd6fa4f789dda2cade90d7605
Edgar Dev <118010644+EdgarrDEV@users.noreply.github.com>
2023-02-18 20:02:28 -0600

    2008

---
+++ parcial01 - copia/lib/menu/Configuracion.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Configuracion extends StatelessWidget {
+  const Configuracion({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Configuracion',
+        style: TextStyle(fontSize: 30), //Estilo de fuente, tamaño, color
+      ),
+    );
+  }
+}
