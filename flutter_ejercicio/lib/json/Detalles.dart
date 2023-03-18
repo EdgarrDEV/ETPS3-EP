cd822e73c23e9225d3847dfe350636cf7b9bc684
Edgar Dev <118010644+EdgarrDEV@users.noreply.github.com>
2023-03-18 00:59:28 -0600

    00:59

--- flutter_ejercicio/lib/json/Detalles.dart
+++ flutter_ejercicio/lib/json/Detalles.dart
@@ -6,31 +6,36 @@ const List Felinos = [
   {
     "img": "assets/images/leon.jpg",
     "title": "León",
-    "description": "El león",
+    "description":
+        "El león (Panthera leo) es un mamífero carnívoro de la familia de los félidos y una de las cinco especies del género Panthera.",
     "color": Color(0xFFf69129),
   },
   {
     "img": "assets/images/tigre.jpg",
-    "title": "León",
-    "description": "El león",
+    "title": "Tigre",
+    "description":
+        "Tigre es el nombre común que reciben los integrantes de la especie Panthera tigris. Este animal mamífero, que está considerado como el felino más grande del planeta, se caracteriza por su pelaje amarillo con rayas negras en el lomo.",
     "color": Color(0xFFf69129),
   },
   {
     "img": "assets/images/puma.jpg",
-    "title": "León",
-    "description": "El león",
+    "title": "Puma",
+    "description":
+        "El puma, león de montaña o león americano (Nombre científico: Puma concolor)​ es un mamífero carnívoro de la familia Felidae nativo de América.",
     "color": Color(0xFFf69129),
   },
   {
     "img": "assets/images/leopardonieves.jpg",
-    "title": "León",
-    "description": "El león",
+    "title": "Leopardo Nieves",
+    "description":
+        "El leopardo de las nieves o irbis​ (Panthera uncia) es una especie de mamífero carnívoro de la familia Felidae propia de las montañas de Asia Central.",
     "color": Color(0xFFf69129),
   },
   {
     "img": "assets/images/leopardo.jpg",
-    "title": "León",
-    "description": "El león",
+    "title": "Leopardo",
+    "description":
+        "El leopardo es un elegante y fuerte felino emparentado con los leones, los tigres y los jaguares. Vive en el África subsahariana, el norte de África, Asia Central, India y China. Sin embargo, en algunos lugares están amenazados, especialmente en las regiones fuera de África.",
     "color": Color.fromARGB(255, 35, 76, 133),
   },
 ];
@@ -38,32 +43,37 @@ const List Felinos = [
 const List Marinos = [
   {
     "img": "assets/images/tiburon.jpg",
-    "title": "León",
-    "description": "El león",
+    "title": "Tiburón",
+    "description":
+        "El  tiburón es un pez cartilaginoso que habita en el mar (aunque algunas especies también nadan en agua dulce) y que se destaca por sus cualidades como depredador. Debido a que su esqueleto está compuesto de cartílago, forma parte del grupo de los condrictios.",
     "color": Color(0xFFf69129),
   },
   {
     "img": "assets/images/delfin.jpg",
-    "title": "León",
-    "description": "El león",
+    "title": "Delfín",
+    "description":
+        "Los delfines son un tipo de mamíferos cetáceos pertenecientes a la familia Delphinidae. Pueden vivir más de 30 años en cautividad y tienen una longitud de 3,5 metros aproximadamente. En la naturaleza, estos nadadores elegantes pueden alcanzar velocidades de más de 30 kilómetros por hora.",
     "color": Color(0xFFf69129),
   },
   {
     "img": "assets/images/peces.jpg",
-    "title": "León",
-    "description": "El león",
+    "title": "Pez",
+    "description":
+        "Los peces (del latín pisces) son animales vertebrados primariamente acuáticos, generalmente ectotérmicos (regulan su temperatura a partir del medio ambiente) y con respiración por branquias.",
     "color": Color(0xFFf69129),
   },
   {
     "img": "assets/images/tortuga.jpg",
-    "title": "León",
-    "description": "El león",
+    "title": "Tortuga",
+    "description":
+        "También conocidos como quelonios, las tortugas son un tipo de reptiles caracterizados por el sólido caparazón que protege sus órganos vitales del que emergen la cabeza, las patas y la cola. Son animales ovíparos que cavan sus nidos en la tierra, donde llevan a cabo la incubación de los huevos.",
     "color": Color(0xFFf69129),
   },
   {
     "img": "assets/images/ballena.jpg",
-    "title": "León",
-    "description": "El león",
+    "title": "Ballena",
+    "description":
+        "Las ballenas conforman la familia de los balaénidos o de las ballenas barbadas. Estas están clasificados en tan solo cuatro especies divididas en dos géneros, Balaena y Eubalaena. Estos animales pueden llegar a medir entre 25 y 32 metros y los ejemplares más grandes pueden llegar a pesar hasta 180 toneladas.",
     "color": Color.fromARGB(255, 35, 76, 133),
   },
 ];
@@ -71,32 +81,37 @@ const List Marinos = [
 const List Mamiferos = [
   {
     "img": "assets/images/zorro.jpg",
-    "title": "León",
-    "description": "El león",
+    "title": "Zorro",
+    "description":
+        "Pertenece a la familia de los cánidos y suelen tener un tamaño pequeño. Su carácter independiente y solitario no ha permitido que hayan sido domesticados, sin embargo en ocasiones son muy útiles para acabar con ciertas plagas en las plantaciones de fruta.",
     "color": Color(0xFFf69129),
   },
   {
     "img": "assets/images/elefante.jpg",
-    "title": "León",
-    "description": "El león",
+    "title": "Elefante",
+    "description":
+        "Son animales mamíferos, exclusivamente herbívoros cuya característica principal –además del gran tamaño– es su larga trompa y sus enormes orejas. Se organizan en manadas de unos 20 miembros, miden casi 4 metros los ejemplares más grandes y pueden llegar a pesar más de 6.000 kilos. Suelen vivir alrededor de 70 años y tienen el periodo de gestación más largo de todos los animales: 22 meses. Nada más nacer, las crías ya pesan más de 100 kilos.",
     "color": Color(0xFFf69129),
   },
   {
     "img": "assets/images/mono.jpg",
-    "title": "León",
-    "description": "El león",
+    "title": "Mono",
+    "description":
+        "Los simios o monos son primates de la infraorden de los haplorrinos. En castellano se suele usar la misma palabra para referirse de igual modo a los monos y a los simios en lenguaje común, incluso para aquellos simios que carecen de cola.",
     "color": Color(0xFFf69129),
   },
   {
     "img": "assets/images/ardilla.jpg",
-    "title": "León",
-    "description": "El león",
+    "title": "Ardilla",
+    "description":
+        "Las ardillas son animales conocidos por todos. Unas 200 especies de ardillas viven en todo el mundo, con excepción de Australia.",
     "color": Color(0xFFf69129),
   },
   {
     "img": "assets/images/canguro.jpg",
-    "title": "León",
-    "description": "El león",
+    "title": "Canguro",
+    "description":
+        "Un canguro es un animal herbívoro y mamífero que forma parte del conjunto de los marsupiales: aquellas especies que incuban a sus crías en una bolsa ventral donde se encuentran las mamas.",
     "color": Color.fromARGB(255, 35, 76, 133),
   },
 ];
